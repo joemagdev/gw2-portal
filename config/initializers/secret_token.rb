c8286f041519ef765927188979b06f786ec16234
Joe Maguire <joemagdev@gmail.com>
2013-08-26 18:56:15 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gw2Portal::Application.config.secret_key_base = '796f618025bdc2b6434f3d1fc344b4c9cf21e51fd800aacfeaa0c9d9320cb2d5cbbc4dc66da4fe9ba14382d6a243ce7d2ae4f8db8dbdff0dba1ddb23df3334f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gw2Portal::Application.config.session_store :cookie_store, key: '_gw2-portal_session'
